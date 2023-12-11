934b58b227bf79181c6fea1acb45d62522f7c4e1
adityathakur-cse <adithakurji@gmail.com>
2023-12-11 14:04:58 +0530

    initial commit

---
+++ sum-of-two-no.py
@@ -0,0 +1,11 @@
+# with pre defined variable
+num1 = 30
+num2 = 40
+sum = num1 + num2
+print(sum)
+
+# with user input
+num1 = float(input("enter 1st no. : "))
+num2 = float(input("enter 2nd no. : "))
+sum = num1 + num2
+print(sum)
